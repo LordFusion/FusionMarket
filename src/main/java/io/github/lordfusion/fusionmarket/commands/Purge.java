71b7d0068d18e776f0164dae8675276f9b9937cb
Lord_Fusion <mgreen9800@gmail.com>
2021-12-07 21:26:19 -0600

    Rewrite Purge command to make it work

--- src/main/java/io/github/lordfusion/fusionmarket/commands/Purge.java
+++ src/main/java/io/github/lordfusion/fusionmarket/commands/Purge.java
@@ -5,13 +5,11 @@ import io.github.lordfusion.fusionmarket.FusionMarket;
 import io.github.lordfusion.fusionmarket.Shop;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
-import org.bukkit.block.Sign;
 import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Purge
 {
@@ -25,64 +23,68 @@ public class Purge
     {
         DataManager dataManager = FusionMarket.getInstance().getDataManager();
         Shop[] allShops = dataManager.getAllShops();
-        ArrayList<Shop> invalidShops = new ArrayList<>();
-        OfflinePlayer player = null;
+        ArrayList<Shop> shopsToVerify = new ArrayList<>();
         
         if (args.length == 1) {
-            player = DataManager.findPlayer(args[0]);
-            if (player == null) {
+            // Check if username is valid
+            OfflinePlayer player = DataManager.findPlayer(args[0]);
+            if (player == null || player.getUniqueId() == null) {
                 BaseCommand.commandFailure(sender, "Invalid player: " + args[0]);
                 return;
             }
+            
+            // Get all shops owned by the username
+            for (Shop shop : allShops)
+                if (shop.getOwner() != null && shop.getOwner().equals(player.getUniqueId()))
+                    shopsToVerify.add(shop);
+        } else {
+            // No username provided; verify all shops
+            shopsToVerify.addAll(Arrays.asList(allShops));
         }
         
-        for (Shop shop : allShops) {
-            // Verify username
-            if (player != null && !shop.getOwner().equals(player.getUniqueId()))
+        // Verify specified shops
+        ArrayList<Shop> shopsToPurge = new ArrayList<>();
+        for (Shop shop : shopsToVerify) {
+            // Verify Unique-ID; all shops must have a UID
+            if (shop.getUniqueId() == null) {
+                FusionMarket.sendConsoleInfo("Purging shop for null UID.");
+                shopsToPurge.add(shop);
                 continue;
-            
-            // Verify chest
-            if (shop.getChestLocation() == null) {
-                invalidShops.add(shop);
-                continue;
-            } else {
-                World world = Bukkit.getServer().getWorld(shop.getChestLocation().getWorldName());
-                if (world == null) {
-                    FusionMarket.sendConsoleWarn("Could not verify chest for shop '" + shop.getUniqueId() + "'.");
-                } else {
-                    if (!DataManager.getChestSubstitutes().contains(world.getBlockAt(shop.getChestLocation().asLocation()).getType())) {
-                        invalidShops.add(shop);
-                        continue;
-                    }
-                }
             }
             
-            // Verify sign
-            if (shop.getSign() == null) {
-                invalidShops.add(shop);
+            // Verify owner; All shops must have an owner, unless it's an AdminShop.
+            if (shop.getOwner() == null && !shop.isAdminShop()) {
+                FusionMarket.sendConsoleInfo("Purging shop for null owner non-admin.");
+                shopsToPurge.add(shop);
                 continue;
-            } else {
-                World world = shop.getSign().getWorld();
-                if (world == null) {
-                    FusionMarket.sendConsoleWarn("Could not verify sign for shop '" + shop.getUniqueId() + "'.");
-                } else {
-                    if (!(world.getBlockAt(shop.getSign().getLocation()).getState() instanceof Sign)) {
-                        invalidShops.add(shop);
-                        continue;
-                    }
-                }
+            }
+            
+            // Verify sign location; All shops must have a sign.
+            if (shop.getSign() == null || shop.getSign().getLocation() == null) {
+                // todo: verify sign exists at location
+                FusionMarket.sendConsoleInfo("Purging shop for missing sign.");
+                shopsToPurge.add(shop);
+                continue;
+            }
+            
+            // Verify chest location; All non-admin shops must have a chest.
+            if (!shop.isAdminShop() && shop.getChestLocation() == null) {
+                // todo: verify chest exists at location
+                FusionMarket.sendConsoleInfo("Purging shop for missing chest.");
+                shopsToPurge.add(shop);
+                continue;
             }
         }
         
-        dataManager.purgeShops(invalidShops.toArray(new Shop[0]));
-        
-        if (invalidShops.size() == 0) {
+        dataManager.purgeShops(shopsToPurge.toArray(new Shop[0]));
+    
+        if (shopsToPurge.size() == 0) {
             TextComponent failure = new TextComponent("0 shops were purged.");
             failure.setColor(ChatColor.RED);
             FusionMarket.sendUserMessage(sender, failure);
             return;
         } else {
-            TextComponent success = new TextComponent(invalidShops.size() + " shops were purged.");
+            TextComponent success = new TextComponent(shopsToPurge.size() + " shops were purged.");
             success.setColor(ChatColor.GREEN);
             FusionMarket.sendUserMessage(sender, success);
             return;
